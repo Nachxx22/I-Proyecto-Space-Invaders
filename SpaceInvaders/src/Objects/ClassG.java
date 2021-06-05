72af78cce96afac0f0949bae16649a256c1aa4a1
Sof-valur <62963590+Sof-valur@users.noreply.github.com>
2021-06-04 21:24:18 -0600

    arbol avl
    Se cambió código para ver si funciona mejor

---
+++ SpaceInvaders/src/Objects/ClassG.java
@@ -0,0 +1,38 @@
+package Objects;
+
+import java.awt.Graphics2D;
+import java.util.Comparator;
+
+import DataStructures.ArbolAvl;
+
+public class ClassG extends  InvaderLine implements  Drawable{
+    public ClassG(int posX, int posY, int speed, int size, int lvl) {
+        super(posX, posY, speed, size, lvl);
+        int counter = 0;
+        this.setEn(new ArbolAvl<Invader>());
+
+        while(counter < size) {
+            String sprite = "Invader_1";
+
+            Invader enemy = new Invader(posX, posY, 50, 50, lvl, speed, sprite);
+            this.getEn().agregar(enemy);
+            counter++;
+        }
+
+        this.setLineClass("ArbolAVL");
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        while (this.getEn().tamano()>0) {
+            this.getEn().contiene(2);
+        }
+
+    }
+
+    @Override
+    public void update(double delta) {
+
+    }
+}
